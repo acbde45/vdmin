6f332d57efbb0a15d51595cef6c4cc3718193a7d
Acbde45 <18342916571@163.com>
2022-05-30 10:54:06 +0800

    refind deleted files

---
+++ packages/cli/lib/compiler/gen-docs-site-desktop-shared.js
@@ -0,0 +1,82 @@
+import { join } from 'path';
+import fse from 'fs-extra';
+import { isDev, pascalize, normalizePath } from '../common/index.js';
+import {
+  DOCS_DIR,
+  getPackageJson,
+  getVdminDocsConfig,
+} from '../common/constants.js';
+
+function formatName(component) {
+  component = pascalize(component);
+
+  return component;
+}
+
+function resolveDocuments(nav) {
+  const docs = [];
+
+  nav.forEach((group) => {
+    group.items.forEach((item) => {
+      docs.push(
+        {
+          name: formatName(item.path),
+          path: join(DOCS_DIR, group.group, item.path, 'README.md'),
+        },
+        {
+          name: formatName(item.path),
+          path: join(DOCS_DIR, group.group, item.path + '.md'),
+        }
+      );
+    });
+  });
+
+  return [...docs.filter((item) => fse.existsSync(item.path))];
+}
+
+function genImportDocuments(items) {
+  return items
+    .map((item) => {
+      const path = normalizePath(item.path);
+      if (isDev()) {
+        return `const ${item.name} = () => import('${path}');`;
+      }
+      return `import ${item.name} from '${path}';`;
+    })
+    .join('\n');
+}
+
+function genExportDocuments(items) {
+  return `export const documents = {
+  ${items.map((item) => item.name).join(',\n  ')}
+};`;
+}
+
+function genVantConfigContent() {
+  const content = getVdminDocsConfig();
+  return 'const config = ' + JSON.stringify(content, null, 2);
+}
+
+function genExportConfig() {
+  return 'export { config };';
+}
+
+function genExportVersion() {
+  return `export const packageVersion = '${getPackageJson().version}';`;
+}
+
+export default async function genSiteDesktopShared() {
+  const docsConfig = await getVdminDocsConfig();
+  const documents = resolveDocuments(docsConfig.site?.nav || []);
+
+  const code = `${genImportDocuments(documents)}
+
+${genVantConfigContent()}
+
+${genExportConfig()}
+${genExportDocuments(documents)}
+${genExportVersion()}
+`;
+
+  return code;
+}
---
+++ packages/cli/lib/compiler/gen-docs-site-mobile-shared.js
@@ -0,0 +1,78 @@
+import { join } from 'path';
+import fse from 'fs-extra';
+import { DOCS_DIR } from '../common/constants.js';
+import {
+  pascalize,
+  removeExt,
+  decamelize,
+  getVdminDocsConfig,
+  normalizePath,
+} from '../common/index.js';
+import { dir } from 'console';
+
+function genImports(demos) {
+  return demos
+    .map((item) => {
+      const path = removeExt(normalizePath(item.path));
+      return `const ${item.name} = () => import('${path}')`;
+    })
+    .join('\n');
+}
+
+function genExports(demos) {
+  return `export const demos = {\n  ${demos
+    .map((item) => item.name)
+    .join(',\n  ')}\n};`;
+}
+
+function genConfig(demos) {
+  const docsConfig = getVdminDocsConfig();
+  const demoNames = demos.map((item) => decamelize(item.name));
+
+  function demoFilter(nav) {
+    return nav.filter((group) => {
+      group.items = group.items.filter((item) =>
+        demoNames.includes(item.path)
+      );
+      return group.items.length;
+    });
+  }
+
+  const { nav } = docsConfig.site;
+  if (nav) {
+    docsConfig.site.nav = demoFilter(nav);
+  }
+
+  return `export const config = ${JSON.stringify(docsConfig, null, 2)}`;
+}
+
+function genCode(nav) {
+  let demos = [];
+
+  nav.forEach((group) => {
+    group.items.forEach((item) => {
+      demos.push(
+        {
+          component: item.path,
+          name: pascalize(item.path),
+          path: join(DOCS_DIR, group.group, item.path, 'demo/index.vue'),
+        },
+      );
+    });
+  });
+
+  demos = [...demos.filter((item) => fse.existsSync(item.path))];
+
+  return `${genImports(demos)}
+
+${genExports(demos)}
+${genConfig(demos)}
+`;
+}
+
+export default function genSiteMobileShared() {
+  const docsConfig = getVdminDocsConfig();
+  const code = genCode(docsConfig.site?.nav || []);
+
+  return code;
+}
